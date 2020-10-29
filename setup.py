690fd345b9575f69af8f25077f165a3532899e22
MrYsLab <MisterYsLab@gmail.com>
2020-10-29 17:51:31 -0400

    Version 0.06

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     packages=['telemetrix_aio'],
     install_requires=['pyserial'],
 
-    version='0.1.0',
+    version='0.2.0',
     description="telemetrix-aio client and server",
 
     author='Alan Yorinks',
