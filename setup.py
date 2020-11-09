8c7d3e761ad7099e0e118c8f279abb9f2f10f0c0
MrYsLab <MisterYsLab@gmail.com>
2020-11-09 11:02:25 -0500

    First Production Release

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     packages=['telemetrix_aio'],
     install_requires=['pyserial'],
 
-    version='0.2.0',
+    version='1.0',
     description="Remotely Control And Monitor Arduino-Core devices",
     long_description=long_description,
     long_description_content_type='text/markdown',
