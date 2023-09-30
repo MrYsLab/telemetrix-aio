d8e02d6641be5e499bd67c81efb04c180800fa33
MrYsLab <MisterYsLab@gmail.com>
2023-09-30 10:40:10 -0400

    Fixed a missing await instruction

--- telemetrix_aio/private_constants.py
+++ telemetrix_aio/private_constants.py
@@ -105,7 +105,7 @@ class PrivateConstants:
 
     DEBUG_PRINT = 99
 
-    TELEMETRIX_AIO_VERSION = "1.21"
+    TELEMETRIX_AIO_VERSION = "1.22"
 
     # reporting control
     REPORTING_DISABLE_ALL = 0
