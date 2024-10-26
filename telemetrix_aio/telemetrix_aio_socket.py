3c54dd3f86b36cb64d298c24429c4bc140e8c1ad
MrYsLab <MisterYsLab@gmail.com>
2024-10-26 11:19:50 -0400

    Optional setting of "read bit" in spi_read_blocking

--- telemetrix_aio/telemetrix_aio_socket.py
+++ telemetrix_aio/telemetrix_aio_socket.py
@@ -1,6 +1,6 @@
 
 """
- Copyright (c) 2020 Alan Yorinks All rights reserved.
+ Copyright (c) 2020-2024 Alan Yorinks All rights reserved.
 
  This program is free software; you can redistribute it and/or
  modify it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE
